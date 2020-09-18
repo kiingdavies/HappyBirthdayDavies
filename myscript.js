edc7cae069aeed1f4f362fba9357e17488a7dc2f
kiingdavies <daviesadedeji007@gmail.com>
2020-09-18 06:07:18 +0100

    initial commit

---
+++ myscript.js
@@ -0,0 +1,207 @@
+window.onload = function () {
+
+    let birthday = document.getElementById("birthday");
+    let section1 = document.getElementById("section1");
+    let button1 = document.getElementById("button1");
+    let section2 = document.getElementById("section2");
+    let cake = document.getElementById("cake");
+    let virat = document.getElementById("virat");
+    let cap = document.getElementById("cap");
+    let mouth = document.getElementById("mouth");
+    let button2 = document.getElementById("button2");
+    let flame1 = document.getElementsByClassName('flame1')[0]
+
+    let section3 = document.getElementById("section3");
+    let item1 = document.getElementById("item1");
+    let item2 = document.getElementById("item2");
+    let item3 = document.getElementById("item3");
+    let item4 = document.getElementById("item4");
+    let item5 = document.getElementById("item5");
+    let item6 = document.getElementById("item6");
+    let item7 = document.getElementById("item7");
+    let item8 = document.getElementById("item8");
+    let item9 = document.getElementById("item9");
+    let item10 = document.getElementById("item10");
+    let item11 = document.getElementById("item11");
+    let item12 = document.getElementById("item12");
+
+    let item13 = document.getElementById("item13");
+    let item14 = document.getElementById("item14");
+    let item15 = document.getElementById("item15");
+    let item16 = document.getElementById("item16");
+    let item17 = document.getElementById("item17");
+    let item18 = document.getElementById("item18");
+    let item19 = document.getElementById("item19");
+    let item20 = document.getElementById("item20");
+    let item21 = document.getElementById("item21");
+    let item22 = document.getElementById("item22");
+    let item23 = document.getElementById("item23");
+    let item24 = document.getElementById("item24");
+    let item25 = document.getElementById("item25");
+    let item26 = document.getElementById("item26");
+    let item27 = document.getElementById("item27");
+
+    button1.onclick = function () {
+
+        section1.style.display = "none";
+        section2.style.display = "block";
+        cake.setAttribute("style", "animation:cake 6s ease-in 0.1s 1;animation-fill-mode:forwards;transform-origin: 20% 40%;");
+        virat.setAttribute("style", "animation:virat 1s linear 8s 1;animation-fill-mode:forwards;");
+
+        mouth.setAttribute("style", " animation:mouth 8s linear 10s 1;z-index:1;");
+
+        cap.setAttribute("style", " animation:bounce-in-top 1.1s both;animation-delay:9s;");
+
+        flame1.setAttribute("style", " animation:flame 3s linear 10s 1;animation-fill-mode:forwards");
+
+
+        button2.setAttribute("style", "animation:bounce-in-top 1.1s both;animation-delay:22s;");
+
+        // section3 starts  ******************************* 
+
+        button2.onclick = function () {
+
+            section1.style.display = "none";
+            section2.style.display = "none";
+            section3.style.display = "block";
+
+            item1.onclick = function () {
+                item1.setAttribute('style', 'animation:item1 1s linear 0.1s 1;animation-fill-mode:forwards;transition: all 0.5s ease;')
+
+            }
+
+
+            item2.onclick = function () {
+                item2.setAttribute('style', 'animation:item2 1s linear 0.1s 1;animation-fill-mode:forwards;transition: all 0.5s ease;')
+
+            }
+
+            item3.onclick = function () {
+                item3.setAttribute('style', 'animation:item3 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+            item4.onclick = function () {
+                item4.setAttribute('style', 'animation:item4 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+            item5.onclick = function () {
+                item5.setAttribute('style', 'animation:item5 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+
+            item6.onclick = function () {
+                item6.setAttribute('style', 'animation:item6 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+
+            item7.onclick = function () {
+                item7.setAttribute('style', 'animation:item7 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+
+            item8.onclick = function () {
+                item8.setAttribute('style', 'animation:item8 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+            item9.onclick = function () {
+                item9.setAttribute('style', 'animation:item9 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+
+            item10.onclick = function () {
+                item10.setAttribute('style', 'animation:item10 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+            item11.onclick = function () {
+                item11.setAttribute('style', 'animation:item11 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+            item12.onclick = function () {
+                item12.setAttribute('style', 'animation:item12 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+
+            item13.onclick = function () {
+                item13.setAttribute('style', 'animation:item13 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+            item14.onclick = function () {
+                item14.setAttribute('style', 'animation:item14 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+            item15.onclick = function () {
+                item15.setAttribute('style', 'animation:item15 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+
+            item16.onclick = function () {
+                item16.setAttribute('style', 'animation:item16 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+            item17.onclick = function () {
+                item17.setAttribute('style', 'animation:item17 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+
+            item18.onclick = function () {
+                item18.setAttribute('style', 'animation:item18 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+
+            item19.onclick = function () {
+                item19.setAttribute('style', 'animation:item19 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+
+            item20.onclick = function () {
+                item20.setAttribute('style', 'animation:item20 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+            item21.onclick = function () {
+                item21.setAttribute('style', 'animation:item21 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+
+            item23.onclick = function () {
+                item23.setAttribute('style', 'animation:item23 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+            item24.onclick = function () {
+                item24.setAttribute('style', 'animation:item24 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+
+            item27.onclick = function () {
+                item27.setAttribute('style', 'animation:item27 1s linear 0.1s 1;animation-fill-mode:forwards')
+
+            }
+
+        }
+
+
+    }
+
+
+};
+
